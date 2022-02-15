72f4b2c077d686154afe600c40a18767a9b49618
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-02-14 20:18:19 -0500

    add toggle for balloon

--- scrimba-challenges/gift-selector/index.js
+++ scrimba-challenges/gift-selector/index.js
@@ -1,17 +1,31 @@
 document.getElementById("btn").addEventListener("click", calculate);
 let costEl = document.getElementById("cost-el");
- 
+let balloon = document.getElementById("balloon-checkbox");
+let balloonEmoji = document.getElementById("balloon-emoji");
+balloon.addEventListener("click", emoji);
+
+
+function emoji() {
+    if (balloonEmoji.style.display === "inline") {
+        balloonEmoji.style.display = "none";
+    }
+    else {balloonEmoji.style.display = "inline"}
+}
+
 
 function calculate() {
     let food = document.getElementById("food-select").value;
     let transport = document.getElementById("transport-select").value;
-    let balloon = document.getElementById("balloon-checkbox");
     let balloonCost
 
     if (balloon.checked) {
         balloonCost = 1;
     }
     else {balloonCost = 0}
+
+    if (food === "" | transport === "") {
+        alert("Please select your options!");
+    }
     
     let totalCost = Number(food) + Number(transport) + Number(balloonCost);
 
