2cd6b48824e008704f890e3f36bb864073da17d3
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-02-15 17:17:19 -0500

    custom from field updates and event listeners

--- scrimba-challenges/greeting-selector/index.js
+++ scrimba-challenges/greeting-selector/index.js
@@ -2,24 +2,64 @@
 
 let greetingDisplay = document.getElementById("greeting-display");
 let customGreeting = document.getElementById("custom-greeting");
-let greetingMessage
+let from = document.getElementById("sender-input");
+let fromToggle = document.getElementById("from-toggle");
+let greetingMessage;
 
 
+// Function for event listener for updating from field
 
-// Function for writing greeting
+function updateFrom() {
+
+    from.addEventListener("input", function() {
+        if (this.value !== "") {
+            fromToggle.innerHTML = "From: " + this.value;
+        }
+        if (this.value === "") {
+            fromToggle.innerHTML = this.value
+        }
+        
+    });
+
+}
+
+
+// Function for event listener for updating custom field
+
+function updateCustom() {
+
+    customGreeting.addEventListener("input", function() {
+        if (this.value !== "") {
+            greetingDisplay.innerHTML = this.value
+        }
+        if (this.value === "") {
+            greetingDisplay.innerHTML = "Type custom greeting below"
+        }
+    });
+
+}
+
+
+// Function for greeting selection on change
 
 function writeGreeting() {
     greetingMessage = document.getElementById("greeting-select").value;
 
-    if (greetingMessage === "Custom greeting") {
-        customGreeting.style.display = "flex";
-        greetingDisplay.textContent = customGreeting.textContent;
-    }
-    else {
-        customGreeting.style.display = "none";
-        greetingDisplay.textContent = greetingMessage;
-    }
-
-    
-    
+        if (greetingMessage === "Custom greeting") {
+            customGreeting.style.display = "flex";
+            greetingDisplay.innerHTML = "Type custom greeting below"
+            updateCustom();
+            updateFrom();
+        }
+            
+        else {
+            customGreeting.style.display = "none";
+            customGreeting.value = "";
+            greetingDisplay.innerHTML = greetingMessage
+            updateCustom();
+            updateFrom();
+        }
 }
+
+
+
