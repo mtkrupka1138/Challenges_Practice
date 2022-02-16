4489d2315fd8ca0728d83ea8ea8511f244eea0a3
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-02-16 16:11:50 -0500

    extra challenge to capitalize all first letters

--- scrimba-challenges/grammar-corrector/index.js
+++ scrimba-challenges/grammar-corrector/index.js
@@ -1,9 +1,16 @@
+// Define variables and event listeners
+
 document.getElementById("btn").addEventListener("click", correct);
 let stringToCorrect = document.getElementById("paragraph").innerHTML;
 let newString = [];
 
-function correct() {
 
+// Function to capitalize first letters of each word 
+// and make all other letters lowercase; keeps spaces and special characters
+// as is
+
+function correct() {
+    
     if (document.getElementById("paragraph").innerHTML === newString.join('')) {
         alert("Refresh the page!")
     }
@@ -14,13 +21,32 @@ function correct() {
                 let firstLetter = splitString[0].toUpperCase();
                 newString.push(firstLetter);
             }
+            else if (splitString[i] === ' ') {
+                newString.push(splitString[i]);
+            }
+            else if (splitString[i-1] === ' ') {
+                let capitalLetter = splitString[i].toUpperCase()
+                newString.push(capitalLetter);
+            }
             else {
                 let otherLetter = splitString[i].toLowerCase();
                 newString.push(otherLetter);
             }
         }
-    
-    document.getElementById("paragraph").innerHTML = newString.join('')
+        // original code for first test case only
+        // let splitString = stringToCorrect.split('');
+        // for (let i = 0; i < splitString.length; i++) {
+        //     if (splitString[i] === splitString[0]) {
+        //         let firstLetter = splitString[0].toUpperCase();
+        //         newString.push(firstLetter);
+        //     }
+        //     else {
+        //         let otherLetter = splitString[i].toLowerCase();
+        //         newString.push(otherLetter);
+        //     }
+        // }
+
+        document.getElementById("paragraph").innerHTML = newString.join('')
     }
-    
+
 }
