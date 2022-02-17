adc62db1e00a4560a368a550d1919744ca1020cd
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-02-17 09:33:19 -0500

    almost

--- scrimba-challenges/emoji-filter/index.js
+++ scrimba-challenges/emoji-filter/index.js
@@ -19,22 +19,28 @@ for (let emoji of emojis) {
 }
 
 let emojiArr = allEmojisP.textContent.split('');
+let concatEmoji = [];
+for (let j = 1; j < emojiArr.length; j++) {
+    if (!(j%2===0)) {
+        let emojiConcat = emojiArr[j-1].concat(emojiArr[j])
+            concatEmoji.push(emojiConcat)
+        }
+    }
+
 let uniqueEmoji = [];
 
 function filterDuplicates() {
-    for (let i = 0; i < emojiArr.length; i++) {
-        if (emojiArr[i] === emojiArr[0]) {
+    for (let i = 0; i < concatEmoji.length; i++) {
+        if (concatEmoji[i] === concatEmoji[0]) {
             uniqueEmoji.push(emojiArr[i]);
-            console.log(emojiArr[i]);
         }
         else {
-            let prevArr = emojiArr.slice(0, i);
-            if (!prevArr.includes(emojiArr[i])) {
-                uniqueEmoji.push(emojiArr[i]);
-                console.log(emojiArr[i]);
+            let prevArr = uniqueEmoji.slice();
+            if (!prevArr.includes(concatEmoji[i])) {
+                uniqueEmoji.push(concatEmoji[i]);
             }
         }
     }
     let joinUnique = uniqueEmoji.join('');
-    return joinUnique;
+    uniqueEmojisP.textContent = joinUnique;
 }
