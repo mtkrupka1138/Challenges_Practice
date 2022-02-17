e90175d470972a607a9191ffa674c2ef61935d10
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-02-17 09:44:50 -0500

    used concat to combine emoji text pairs for single emoji

--- scrimba-challenges/emoji-filter/index.js
+++ scrimba-challenges/emoji-filter/index.js
@@ -21,7 +21,7 @@ for (let emoji of emojis) {
 let emojiArr = allEmojisP.textContent.split('');
 let concatEmoji = [];
 for (let j = 1; j < emojiArr.length; j++) {
-    if (!(j%2===0)) {
+    if ( !(j%2===0) ) {
         let emojiConcat = emojiArr[j-1].concat(emojiArr[j])
             concatEmoji.push(emojiConcat)
         }
@@ -31,16 +31,15 @@ let uniqueEmoji = [];
 
 function filterDuplicates() {
     for (let i = 0; i < concatEmoji.length; i++) {
-        if (concatEmoji[i] === concatEmoji[0]) {
-            uniqueEmoji.push(emojiArr[i]);
-        }
-        else {
-            let prevArr = uniqueEmoji.slice();
-            if (!prevArr.includes(concatEmoji[i])) {
-                uniqueEmoji.push(concatEmoji[i]);
-            }
+        let prevArr = uniqueEmoji.slice();
+        if (!prevArr.includes(concatEmoji[i])) {
+            uniqueEmoji.push(concatEmoji[i]);
         }
     }
     let joinUnique = uniqueEmoji.join('');
-    uniqueEmojisP.textContent = joinUnique;
+    if (uniqueEmojisP.textContent !== joinUnique) {
+        uniqueEmojisP.textContent = joinUnique;
+    }
+    else { alert("Refresh the page to restart!") }
+    
 }
