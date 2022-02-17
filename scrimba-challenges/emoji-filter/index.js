5c235175dc96c4b66a28a9b9be8cf0e78c7c0e26
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-02-17 11:35:04 -0500

    added basic sort functionality

--- scrimba-challenges/emoji-filter/index.js
+++ scrimba-challenges/emoji-filter/index.js
@@ -1,4 +1,10 @@
-document.getElementById("btn").addEventListener("click", filterDuplicates)
+// Event listeners for button clicks
+
+document.getElementById("btn-filter").addEventListener("click", filterDuplicates)
+document.getElementById("btn-sort").addEventListener("click", sortEmojis)
+
+
+// Original emoji array input 
 
 const emojis = [
     "💘","💘","🌞","🧡","🍇",
@@ -11,6 +17,9 @@ const emojis = [
     "💕","🍇","💘"
     ]
     
+
+// Variables for paragraph elements and for loop to add emojis to text content
+
 const allEmojisP = document.getElementById("all-emojis-p")
 const uniqueEmojisP = document.getElementById("unique-emojis-p")
 
@@ -18,18 +27,28 @@ for (let emoji of emojis) {
     allEmojisP.textContent += emoji
 }
 
+
+// Concatenate two portions of each emoji and push to new array
+
 let emojiArr = allEmojisP.textContent.split('');
 let concatEmoji = [];
 for (let j = 1; j < emojiArr.length; j++) {
     if ( !(j%2===0) ) {
         let emojiConcat = emojiArr[j-1].concat(emojiArr[j])
             concatEmoji.push(emojiConcat)
-        }
     }
+}
+
+
+// Initiate array to store unique emojis
 
 let uniqueEmoji = [];
 
+
+// Function to find/filter duplicates
+
 function filterDuplicates() {
+
     for (let i = 0; i < concatEmoji.length; i++) {
         let prevArr = uniqueEmoji.slice();
         if (!prevArr.includes(concatEmoji[i])) {
@@ -43,3 +62,22 @@ function filterDuplicates() {
     else { alert("Refresh the page to restart!") }
     
 }
+
+
+// Function to sort by emoji type (used simplest solution that does not consider original order)
+
+function sortEmojis() {
+
+    let sortedEmojis = concatEmoji.sort();
+
+    let joinUnique = sortedEmojis.join('');
+    if (uniqueEmojisP.textContent !== joinUnique) {
+        uniqueEmojisP.textContent = joinUnique;
+    }
+    else { alert("Refresh the page to restart!") }
+
+}
+
+
+// Note: you can press the sort button after the filter button and vice versa
+// but you cannot press either one two times in a row; alert will appear to refresh the page
