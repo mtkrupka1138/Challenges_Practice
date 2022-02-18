e8df821a6ca6d39b4a89e783623863ba1ef553e4
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-02-17 21:52:16 -0500

    added functions to retrieve and update colors

--- scrimba-challenges/heart-customize/index.js
+++ scrimba-challenges/heart-customize/index.js
@@ -1,7 +1,33 @@
-const body = document.body
-document.getElementById("btn").addEventListener("click", changeColor)
+// Declare variables and event listener for button click
+
+const body = document.body;
+const heart = document.getElementById("heart");
+const heartElement = document.querySelector('.heart');
+document.getElementById("btn").addEventListener("click", changeColor);
+
+
+// Function to update the background colors of body and heart elements (changes at same time)
+
+function updateColor(backgroundColor) {
+
+    body.style.setProperty('background-color', backgroundColor.colors[0].value);
+    heart.style.setProperty('background-color', backgroundColor.colors[1].value);
+    document.documentElement.style.setProperty("--pseudo", backgroundColor.colors[1].value);
+
+    colorHeartAfter = backgroundColor.colors[1].value;
+
+}
+
+
+// Async/await function to retrieve two (2) colors from API random color generator and provide to update function
 
 async function changeColor() {
-   // 1. Call Scrimba's color API (https://apis.scrimba.com/hexcolors/) to retrieve one random color.
-   // 2. Use that color to update the background color of the body.
+
+    let response = await fetch ('https://apis.scrimba.com/hexcolors/?count=2');
+    let color = await response.json();
+    updateColor(color);
+
 }
+
+
+
