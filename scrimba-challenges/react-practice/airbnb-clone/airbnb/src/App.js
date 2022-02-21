2968a7492f8dae9ed763451fc1cbbddfc2a7d021
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-02-20 20:04:58 -0500

    card mapping

--- scrimba-challenges/react-practice/airbnb-clone/airbnb/src/App.js
+++ scrimba-challenges/react-practice/airbnb-clone/airbnb/src/App.js
@@ -6,19 +6,25 @@ import "./style.css";
 
 
 export default function App() {
-            
-    return (
-        <div>
-            <Navbar />
-            <Hero />
-            <Card 
-                img="katie-zaferes.png"
-                rating="5.0"
-                reviewCount={6}
-                location="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />
-        </div>
-    )
+
+  const cards = data.map(item => {
+  return (
+    <Card 
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+    />
+  )
+  })        
+
+  return (
+  <div>
+    <Navbar />
+    {cards}
+  </div>
+  
+  )
 }
