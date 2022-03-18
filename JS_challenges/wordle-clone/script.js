0d97ddde1735bb705c100ce654f51e2484f59595
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-03-18 11:35:12 -0400

    variables

--- JS_challenges/wordle-clone/script.js
+++ JS_challenges/wordle-clone/script.js
@@ -1,3 +1,14 @@
+// Global variables
+
+const totalGuessCount = 6;
+let currentGuessCount = 0;
+let wordInProgress = [];
+let currentLetter = 0;
+let correctWord = ;
+
+
+// Event listeners for physical and on-screen keyboards 
+
 document.getElementsByClassName("key").addEventListener
 
 addEventListener("keyup", e)
