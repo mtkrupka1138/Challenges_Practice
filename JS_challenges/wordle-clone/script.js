8f741cc50c2f00baf0f1e94251b654a4d3011962
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-03-21 11:46:57 -0400

    start functions for keyboard events

--- JS_challenges/wordle-clone/script.js
+++ JS_challenges/wordle-clone/script.js
@@ -4,11 +4,27 @@ const totalGuessCount = 6;
 let currentGuessCount = 0;
 let wordInProgress = [];
 let currentLetter = 0;
-let correctWord = ;
+// let correctWord = ;
 
 
 // Event listeners for physical and on-screen keyboards 
 
-document.getElementsByClassName("key").addEventListener
+// on-screen keyboard
+document.getElementById("keyboard").addEventListener("click", screenKeyboard);
 
-addEventListener("keyup", e)
+function screenKeyboard(e) {
+    console.log(e.target.textContent);
+}
+
+
+// physical keyboard
+addEventListener("keyup", physicalKeyboard);
+
+function physicalKeyboard(e) {
+    console.log(e.key);
+}
+
+
+// what happens when key is called 
+// letter needs to be added to wordInProgress
+// need indices 0-4 filled for each guess count
