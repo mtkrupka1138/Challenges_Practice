7d7b3e53a6145883f7c9bf27a336f6fc425cb978
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-04-19 18:14:28 -0400

    animation

--- scrimba-challenges/recycle/index.js
+++ scrimba-challenges/recycle/index.js
@@ -1,10 +1,10 @@
-const recycleBtn = document.getElementById("recycle-btn")
-const emptyBin = document.getElementById("empty-btn")
-recycleBtn.addEventListener("click", recycle)
-emptyBin.addEventListener("click", empty)
-const recycleSelect = document.getElementById("recycle-select")
-const recycleBin = document.getElementById("recycle-bin")
-const infoParagraph = document.getElementById("info-paragraph")
+const recycleBtn = document.getElementById("recycle-btn");
+const emptyBin = document.getElementById("empty-btn");
+recycleBtn.addEventListener("click", recycle);
+emptyBin.addEventListener("click", empty);
+const recycleSelect = document.getElementById("recycle-select");
+const recycleBin = document.getElementById("recycle-bin");
+const infoParagraph = document.getElementById("info-paragraph");
 
 let count = 0;
 
@@ -12,19 +12,15 @@ function recycle() {
     let itemToRecycle = recycleSelect.value;
 
     if (count < 10) {
-        recycleBin.innerHTML += itemToRecycle;
+        recycleBin.innerHTML += 
+        `<div class="div-item" id="${count}">${itemToRecycle}</div>`;
+        document.getElementById(`${count}`).classList.add("bounce");
         count += 1;
     }
     else {alert("You must empty the recycle bin!")}
-    
 }
 
 function empty() {
     recycleBin.innerHTML = '';
     count = 0;
 }
-
-
-
-
-// stretch goal 3️⃣: Animate adding the items to the bin.
