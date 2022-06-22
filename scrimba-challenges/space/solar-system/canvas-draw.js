4cfa240590a818919e55f10211765c159891e572
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-06-21 21:00:36 -0400

    concentric opposite direction circle animation

--- scrimba-challenges/space/solar-system/canvas-draw.js
+++ scrimba-challenges/space/solar-system/canvas-draw.js
@@ -1,23 +1,38 @@
-// requestAnimationFrame Shim
+
+
+
+   
+  
+for (let i=1; i<10; i++) {
+
+    // requestAnimationFrame Shim
 (function() {
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
                                 window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
     window.requestAnimationFrame = requestAnimationFrame;
-  })();
+    })();
+
+   let canvas = document.getElementById(`${i}`);
+   let context = canvas.getContext('2d');
+   let x = canvas.width / 2;
+   let y = canvas.height / 2;
+   let radius = i * 40;
+   const endPercent = 101;
+   let curPerc = 0;
+   let counterClockwise = false;
+   let startAngle = 0;
+   let endAngle = Math.PI * 2;
+   if (i % 2 === 0) {
+    counterClockwise = true;
+    endAngle = -Math.PI * 2;
+
+   }
    
+//    let circ = Math.PI * 2;
+//    let quart = Math.PI / 2;
   
-  var canvas = document.getElementById('myCanvas');
-   var context = canvas.getContext('2d');
-   var x = canvas.width / 2;
-   var y = canvas.height / 2;
-   var radius = 75;
-   var endPercent = 105;
-   var curPerc = 0;
-   var counterClockwise = false;
-   var circ = Math.PI * 2;
-   var quart = Math.PI / 2;
   
-   context.lineWidth = 10;
+   context.lineWidth = 5;
    context.strokeStyle = '#ad2323';
    context.shadowOffsetX = 0;
    context.shadowOffsetY = 0;
@@ -25,11 +40,10 @@
    context.shadowColor = '#656565';
   
   
-   function animate(current) {
-    console.log("current:", current)
+   function animate(percent) {
        context.clearRect(0, 0, canvas.width, canvas.height);
        context.beginPath();
-       context.arc(x, y, radius, -(quart), ((circ) * current) - quart, false);
+       context.arc(x, y, radius, startAngle, endAngle * percent, counterClockwise);
        context.stroke();
        curPerc++;
        if (curPerc < endPercent) {
@@ -38,5 +52,9 @@
            });
        }
    }
+   
+
   
    animate();
+
+}
