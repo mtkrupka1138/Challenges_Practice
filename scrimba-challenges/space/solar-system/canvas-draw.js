c0086f043795f73b60ae01087d8daf2fa525d225
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-06-22 20:07:30 -0400

    test out adding a rotating image

--- scrimba-challenges/space/solar-system/canvas-draw.js
+++ scrimba-challenges/space/solar-system/canvas-draw.js
@@ -1,6 +1,7 @@
 
 
 
+
    
   
 for (let i=1; i<10; i++) {
@@ -27,9 +28,6 @@ for (let i=1; i<10; i++) {
     endAngle = -Math.PI * 2;
 
    }
-   
-//    let circ = Math.PI * 2;
-//    let quart = Math.PI / 2;
   
   
    context.lineWidth = 5;
@@ -58,3 +56,50 @@ for (let i=1; i<10; i++) {
    animate();
 
 }
+
+
+const sun = document.getElementById("sun")
+sun.setAttribute("width", "64")
+sun.setAttribute("height", "64")
+
+function init() {
+    window.requestAnimationFrame(draw)
+}
+
+function draw(percent) {
+       let x = 150;
+   let y = 150;
+   let radius = 40;
+   const endPercent = 101;
+   let curPerc = 0;
+   let startAngle = 0;
+   let endAngle = Math.PI * 2;
+
+    const ctx = document.getElementById('canvas').getContext('2d');
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.clearRect(0, 0, 300, 300); // clear canvas
+//     ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
+//   ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
+  ctx.save();
+   ctx.translate(150, 150);
+     const time = new Date();
+     ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
+    ctx.translate(105, 0);
+    ctx.drawImage(sun, -15, -15, 10, 10);
+    ctx.save();
+    ctx.restore()
+    ctx.restore();
+
+    ctx.beginPath();
+    ctx.arc(x, y, 105, startAngle, endAngle, false);
+       ctx.stroke();
+    //    curPerc++;
+    //    if (curPerc < endPercent) {
+    //        requestAnimationFrame(function () {
+    //            draw(curPerc / 100)
+    //        });
+    //    }
+    window.requestAnimationFrame(draw)
+}
+
+init();
