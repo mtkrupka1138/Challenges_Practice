4ebd55b09a60e39d4a49f15f96591bd2683e52ec
Meghan Krupka <mkrupka.hdm@gmail.com>
2023-01-06 19:33:21 -0500

    layout and css updates, start JS

--- multi-step-form/script.js
+++ multi-step-form/script.js
@@ -1,2 +1,16 @@
 // document element variables
-// event listeners
+const btnsNext = document.querySelectorAll(".btn-next");
+const btnsPrev = document.querySelectorAll(".btn-prev");
+const stepProgress = document.getElementById("steps-overview");
+const formSteps = document.querySelectorAll(".form-step");
+const numSteps = document.querySelectorAll(".step");
+
+let stepCount = 0;
+
+// next buttons acton
+
+// previous buttons action
+
+// function for updating visible form
+
+// function for updating steps
